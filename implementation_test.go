a667d136ac874cd139d64f2e4908be37827a8ff4
Michael Chirozidi <maikl.tigriv@gmail.com>
2023-03-19 19:45:39 +0100

    The repository has been edited for option #8 (PostfixToInfix, gocheck) Also added tests implementation

--- implementation_test.go
+++ implementation_test.go
@@ -2,22 +2,39 @@ package lab2
 
 import (
 	"fmt"
+	. "gopkg.in/check.v1"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestPostfixToInfix(c *C) {
+	res, err := PostfixToInfix("2 2 +")
+	c.Assert(res, Equals, "2 + 2")
+
+	res, err = PostfixToInfix("1 2 + 4 * 3 +")
+	c.Assert(res, Equals, "(1 + 2) * 4 + 3")
+
+	res, err = PostfixToInfix("9 6 3 / - 4 2 - 5 * + 8 4 / + 10 2 + 2 ^ 3 / - 2 * -")
+	c.Assert(res, Equals, "9 - 6 / 3 + (4 - 2) * 5 + 8 / 4 - ( - (10 + 2) ^ 2 / 3) * 2")
+
+	res, err = PostfixToInfix(" '''pi3.14''' 2 +/")
+	c.Assert(err, ErrorMatches, "invalid input expression")
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExamplePostfixToInfix() {
+	res, err := PostfixToInfix("2 2 +")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		panic(err)
+	}
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 2 + 2
 }
